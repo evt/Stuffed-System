8b67feca03e415cc0f9c46c8791a3d30d655e94b
Sergey <eycher@gmail.com>
2010-02-05 16:35:14 +0300

    Added new variable modifiers to the template engine — encode_base64 and decode_base64.

---
+++ private/system/lib/Stuffed/System/Template/Modifier/decode_base64.pm
@@ -0,0 +1,40 @@
+# ============================================================================
+#
+#                        ___
+#                    ,yQ$SSS$Q',      ,'yQQQL
+#                  j$$"`     `?$'  ,d$P"```'$$,
+#           i_L   I$;            `$``       `$$,
+#                                 `          I$$
+#           .:yQ$$$$,            ;        _,d$$'
+#        ,d$$P"^```?$b,       _,'  ;  ,:d$$P"
+#     ,d$P"`        `"?$$Q#QPw`    $d$$P"`
+#   ,$$"         ;       ``       ;$?'
+#   $$;        ,dI                I$;
+#   `$$,    ,d$$$`               j$I
+#     ?$S#S$P'j$'                $$;         Copyright (c) Stuffed Guys
+#       `"`  j$'  __....,,,.__  j$I              www.stuffedguys.org
+#           j$$'"``           ',$$
+#           I$;               ,$$'
+#           `$$,         _.:u$$:`
+#             "?$$Q##Q$$SP$"^`
+#                `````
+#
+# ============================================================================
+
+use strict;
+use vars qw($defs);
+
+$defs->{decode_base64} = {
+	handler 			=> \&decode_base64,
+	version 			=> 1.0,
+	before_encode_html	=> 1,
+};
+
+sub decode_base64 {
+	my ($self, $var) = @_;
+	return '' if false($var);
+	$self->{template}->add_to_top("require MIME::Base64;");
+	return "MIME::Base64::decode_base64($var)";
+}
+
+1;
---
+++ private/system/lib/Stuffed/System/Template/Modifier/encode_base64.pm
@@ -0,0 +1,40 @@
+# ============================================================================
+#
+#                        ___
+#                    ,yQ$SSS$Q',      ,'yQQQL
+#                  j$$"`     `?$'  ,d$P"```'$$,
+#           i_L   I$;            `$``       `$$,
+#                                 `          I$$
+#           .:yQ$$$$,            ;        _,d$$'
+#        ,d$$P"^```?$b,       _,'  ;  ,:d$$P"
+#     ,d$P"`        `"?$$Q#QPw`    $d$$P"`
+#   ,$$"         ;       ``       ;$?'
+#   $$;        ,dI                I$;
+#   `$$,    ,d$$$`               j$I
+#     ?$S#S$P'j$'                $$;         Copyright (c) Stuffed Guys
+#       `"`  j$'  __....,,,.__  j$I              www.stuffedguys.org
+#           j$$'"``           ',$$
+#           I$;               ,$$'
+#           `$$,         _.:u$$:`
+#             "?$$Q##Q$$SP$"^`
+#                `````
+#
+# ============================================================================
+
+use strict;
+use vars qw($defs);
+
+$defs->{encode_base64} = {
+	handler 			=> \&encode_base64,
+	version 			=> 1.0,
+	before_encode_html	=> 1,
+};
+
+sub encode_base64 {
+	my ($self, $var) = @_;
+	return '' if false($var);
+	$self->{template}->add_to_top("require MIME::Base64;");
+	return "MIME::Base64::encode_base64($var)";
+}
+
+1;
