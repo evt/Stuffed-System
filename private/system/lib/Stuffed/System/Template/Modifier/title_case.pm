a32b7980e0f217ceed872dfb085ef582fb553fae
Sergey <eycher@gmail.com>
2010-06-07 16:10:14 +0400

    Added support for title_case variable modifier to templates.

---
+++ private/system/lib/Stuffed/System/Template/Modifier/title_case.pm
@@ -0,0 +1,39 @@
+# ============================================================================
+#
+#                        ___
+#                    ,yQ$SSS$Q',      ,'yQQQL
+#                  j$$"`     `?$'  ,d$P"```'$$,
+#           i_L   I$;            `$``       `$$,
+#                                 `          I$$
+#           .:yQ$$$$,            ;        _,d$$'
+#        ,d$$P"^```?$b,       _,'  ;  ,:d$$P"
+#     ,d$P"`        `"?$$Q#QPw`    $d$$P"`
+#   ,$$"         ;       ``       ;$?'
+#   $$;        ,dI                I$;
+#   `$$,    ,d$$$`               j$I
+#     ?$S#S$P'j$'                $$;         Copyright (c) Stuffed Guys
+#       `"`  j$'  __....,,,.__  j$I              www.stuffedguys.org
+#           j$$'"``           ',$$
+#           I$;               ,$$'
+#           `$$,         _.:u$$:`
+#             "?$$Q##Q$$SP$"^`
+#                `````
+#
+# ============================================================================
+
+use strict;
+use vars qw($defs);
+
+$defs->{title_case} = {
+	handler 			=> \&title_case,
+	version 			=> 1.0,
+	before_encode_html	=> undef,
+};
+
+sub title_case {
+	my ($self, $var) = @_;
+	return '' if false($var);
+	return "Stuffed::System::Utils::title_case($var)";
+}
+
+1;
